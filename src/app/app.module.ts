090abb2c9eacb6ab4638d594dc673b2417eac538
root <root@ubuntu-bionic-1.us-central1-a.c.mycloudserver-251014.internal>
2019-09-21 17:23:51 +0000

    generated main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainTableComponent } from './main-table/main-table.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainTableComponent,
+    MainMenuComponent,
+    MainSidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/main-table/main-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-table',
+  templateUrl: './main-table.component.html',
+  styleUrls: ['./main-table.component.css']
+})
+export class MainTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
